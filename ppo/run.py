bf35d89ce52cc269fea796ff6851b7f177662b74
kazukazupi <22kazuaki22@gmail.com>
2023-12-24 00:06:16 +0900

    implemented individual

--- ppo/run.py
+++ ppo/run.py
@@ -1,6 +1,4 @@
-# import os, sys
 import os
-# sys.path.insert(1, os.path.join(sys.path[0], 'externals', 'pytorch_a2c_ppo_acktr_gail'))
 
 import numpy as np
 import time
@@ -8,13 +6,14 @@ import csv
 from collections import deque
 import torch
 
+import evogym.envs
+
 from ppo import utils
 from ppo.evaluate import evaluate
 from ppo.envs import make_vec_envs
-
+from utils.config import Config
 
 from a2c_ppo_acktr import algo
-from a2c_ppo_acktr.algo import gail
 from a2c_ppo_acktr.model import Policy
 from a2c_ppo_acktr.storage import RolloutStorage
 
@@ -22,115 +21,92 @@ from a2c_ppo_acktr.storage import RolloutStorage
 # https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail
 
 def run_ppo(
-    structure:np.ndarray,
+    body:np.ndarray,
     connections:np.ndarray,
-    saving_dir:str,
-    parent_robot_dir:str,
-    args,
-    en_print=True):
+    saving_dir:str):
 
-    termination_condition = utils.TerminationCondition(args.max_iters)
+    termination_condition = utils.TerminationCondition(Config.max_iters)
 
-    if en_print:
-        print(f'Starting training on \n{structure}\nat {saving_dir}...\n')
+    if Config.print_en: print(f'Starting training on \n{body}\nat {saving_dir}...\n')
 
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed_all(args.seed)
+    torch.manual_seed(Config.seed)
+    torch.cuda.manual_seed_all(Config.seed)
 
-    if args.cuda and torch.cuda.is_available() and args.cuda_deterministic:
+    if Config.cuda and torch.cuda.is_available() and Config.cuda_deterministic:
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
-    # ----------------------
-    #  saving information
-    # ----------------------
-
-    # make saving_dir
-    assert(not os.path.exists(saving_dir))
-    os.makedirs(saving_dir)
+    # ---------------------
+    #  1. prepare logging
+    # ---------------------
+    assert os.path.exists(saving_dir)
 
     # save log at saving_dir/log.csv
-    csv_f_path = os.path.join(saving_dir, 'log.csv')
-    if en_print:
-        print("write log at " + csv_f_path)
-    with open(csv_f_path, 'w') as f:
+    csv_file = os.path.join(saving_dir, 'log.csv')
+    if Config.print_en: print("write log at " + csv_file)
+    with open(csv_file, 'w') as f:
         writer = csv.writer(f)
-        writer.writerow(['Updates', 'num episodes', 'num tipesteps', 'mean', 'median', 'min', 'max'])
+        writer.writerow([
+            'Updates', 'num episodes', 'num timesteps', 'mean', 'median', 'min', 'max']
+        )
 
-    # save robot info at saving_dir/robot_dir
-    robot_dir_path = os.path.join(saving_dir, 'robot_dir')
-    assert(not os.path.exists(robot_dir_path))
-    os.makedirs(robot_dir_path)
-    np.save(os.path.join(robot_dir_path, 'structure.npy'), structure)
-    np.save(os.path.join(robot_dir_path, 'connections.npy'), connections)
-    parameter_f_path = os.path.join(robot_dir_path, 'parameter.pt')
+    # save neural network
+    actor_critic_file = os.path.join(saving_dir, 'actor_critic.pt')
+    if Config.print_en:
+        print(f'save actor_critic at {actor_critic_file}')
 
-    if en_print:
-        print(f'save parameter at {parameter_f_path}')
-
+    # ------------------------------------
+    #  2. get environment and controller
+    # ------------------------------------
     torch.set_num_threads(1)
-    device = torch.device("cuda:0" if args.cuda else "cpu")
+    device = torch.device("cuda:0" if Config.cuda else "cpu")
+    log_dir = saving_dir
 
-    envs = make_vec_envs(args.env_name, (structure, connections), args.seed, args.num_processes,
-                         args.gamma, robot_dir_path, device, False)
+    envs = make_vec_envs(
+        env_name = Config.env_name,
+        robot_structure= (body, connections),
+        seed= Config.seed,
+        num_processes = Config.num_processes,
+        gamma = Config.gamma, 
+        log_dir = log_dir,
+        device = device,
+        allow_early_resets = False
+    )
 
     actor_critic = Policy(
         envs.observation_space.shape,
         envs.action_space,
-        base_kwargs={'recurrent': args.recurrent_policy})
-    if parent_robot_dir is not None:
-        assert(args.inherit_flag)
-        utils.inherit_policy(actor_critic, structure, parent_robot_dir, args.env_name)
-        if en_print: print(f'inherit policy from {parent_robot_dir}')
+        base_kwargs={'recurrent': Config.recurrent_policy}
+    )
+    
+    # if parent_robot_dir is not None:
+    #     assert(Config.inherit_en)
+    #     utils.inherit_policy(actor_critic, body, parent_robot_dir, Config.env_name)
+    #     if Config.print_en: print(f'inherit policy from {parent_robot_dir}')
+
     actor_critic.to(device)
 
-    if args.algo == 'a2c':
-        print('Warning: this code has only been tested with ppo.')
-        agent = algo.A2C_ACKTR(
-            actor_critic,
-            args.value_loss_coef,
-            args.entropy_coef,
-            lr=args.lr,
-            eps=args.eps,
-            alpha=args.alpha,
-            max_grad_norm=args.max_grad_norm)
-    elif args.algo == 'ppo':
-        agent = algo.PPO(
-            actor_critic,
-            args.clip_param,
-            args.ppo_epoch,
-            args.num_mini_batch,
-            args.value_loss_coef,
-            args.entropy_coef,
-            lr=args.lr,
-            eps=args.eps,
-            max_grad_norm=args.max_grad_norm)
-    elif args.algo == 'acktr':
-        print('Warning: this code has only been tested with ppo.')
-        agent = algo.A2C_ACKTR(
-            actor_critic, args.value_loss_coef, args.entropy_coef, acktr=True)
+    # ----------------------
+    #  3. train controller
+    # ----------------------
 
-    if args.gail:
-        assert len(envs.observation_space.shape) == 1
-        discr = gail.Discriminator(
-            envs.observation_space.shape[0] + envs.action_space.shape[0], 100,
-            device)
-        file_name = os.path.join(
-            args.gail_experts_dir, "trajs_{}.pt".format(
-                args.env_name.split('-')[0].lower()))
+    agent = algo.PPO(
+        actor_critic,
+        Config.clip_param,
+        Config.ppo_epoch,
+        Config.num_mini_batch,
+        Config.value_loss_coef,
+        Config.entropy_coef,
+        lr=Config.lr,
+        eps=Config.eps,
+        max_grad_norm=Config.max_grad_norm)
 
-        expert_dataset = gail.ExpertDataset(
-            file_name, num_trajectories=4, subsample_frequency=20)
-        drop_last = len(expert_dataset) > args.gail_batch_size
-        gail_train_loader = torch.utils.data.DataLoader(
-            dataset=expert_dataset,
-            batch_size=args.gail_batch_size,
-            shuffle=True,
-            drop_last=drop_last)
-
-    rollouts = RolloutStorage(args.num_steps, args.num_processes,
-                              envs.observation_space.shape, envs.action_space,
-                              actor_critic.recurrent_hidden_state_size)
+    rollouts = RolloutStorage(
+        Config.num_steps,
+        Config.num_processes,
+        envs.observation_space.shape, envs.action_space,
+        actor_critic.recurrent_hidden_state_size
+    )
 
     obs = envs.reset()
     rollouts.obs[0].copy_(obs)
@@ -141,23 +117,22 @@ def run_ppo(
 
     start = time.time()
     num_updates = int(
-        args.num_env_steps) // args.num_steps // args.num_processes
+        Config.num_env_steps) // Config.num_steps // Config.num_processes
 
     rewards_tracker = []
     avg_rewards_tracker = []
     sliding_window_size = 10
     max_determ_avg_reward = float('-inf')
 
-    # end when (j == min{tc.max_iters, num_updates})
+    # end when j == min{tc.max_iters, num_updates}
     for j in range(num_updates):
 
-        if args.use_linear_lr_decay:
+        if Config.use_linear_lr_decay:
             # decrease learning rate linearly
             utils.update_linear_schedule(
-                agent.optimizer, j, num_updates,
-                agent.optimizer.lr if args.algo == "acktr" else args.lr)
+                agent.optimizer, j, num_updates, Config.lr)
 
-        for step in range(args.num_steps):
+        for step in range(Config.num_steps):
             # Sample actions
             with torch.no_grad():
                 value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
@@ -182,8 +157,7 @@ def run_ppo(
             masks = torch.FloatTensor(
                 [[0.0] if done_ else [1.0] for done_ in done])
             bad_masks = torch.FloatTensor(
-                [[0.0] if 'bad_transition' in info.keys() else [1.0]
-                 for info in infos])
+                [[0.0] if 'bad_transition' in info.keys() else [1.0] for info in infos])
             rollouts.insert(obs, recurrent_hidden_states, action,
                             action_log_prob, value, reward, masks, bad_masks)
 
@@ -192,34 +166,23 @@ def run_ppo(
                 rollouts.obs[-1], rollouts.recurrent_hidden_states[-1],
                 rollouts.masks[-1]).detach()
 
-        if args.gail:
-            if j >= 10:
-                envs.venv.eval()
-
-            gail_epoch = args.gail_epoch
-            if j < 10:
-                gail_epoch = 100  # Warm up
-            for _ in range(gail_epoch):
-                discr.update(gail_train_loader, rollouts,
-                             utils.get_vec_normalize(envs)._obfilt)
-
-            for step in range(args.num_steps):
-                rollouts.rewards[step] = discr.predict_reward(
-                    rollouts.obs[step], rollouts.actions[step], args.gamma,
-                    rollouts.masks[step])
-
-        rollouts.compute_returns(next_value, args.use_gae, args.gamma,
-                                 args.gae_lambda, args.use_proper_time_limits)
+        rollouts.compute_returns(
+            next_value,
+            Config.use_gae,
+            Config.gamma,
+            Config.gae_lambda,
+            Config.use_proper_time_limits
+        )
 
         value_loss, action_loss, dist_entropy = agent.update(rollouts)
 
         rollouts.after_update()
-     
+
         # print status
-        if j % args.log_interval == 0 and len(episode_rewards) > 1:
-            total_num_steps = (j + 1) * args.num_processes * args.num_steps
+        if j % Config.log_interval == 0 and len(episode_rewards) > 1:
+            total_num_steps = (j + 1) * Config.num_processes * Config.num_steps
             end = time.time()
-            if en_print:
+            if Config.print_en:
                 print(
                     "Updates {}, num episodes {}, num timesteps {}, FPS {} \n Last {} training episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n"
                         .format(j, num_episdoes, total_num_steps,
@@ -229,33 +192,45 @@ def run_ppo(
                                 np.max(episode_rewards), dist_entropy, value_loss,
                                 action_loss))
             
-            with open(csv_f_path, 'a') as f:
+            with open(csv_file, 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow([j, num_episdoes, total_num_steps, round(np.mean(episode_rewards), 2), round(np.median(episode_rewards), 2), round(np.min(episode_rewards), 2), round(np.max(episode_rewards), 2)])
         
         # evaluate the controller and save it if it does the best so far
-        if (len(episode_rewards) > 1 and (j % args.eval_interval == 0 or termination_condition(j))):
+        if (len(episode_rewards) > 1 and (j % Config.eval_interval == 0 or termination_condition(j))):
             
             obs_rms = utils.get_vec_normalize(envs).obs_rms
-            determ_avg_reward = evaluate(args.num_evals, actor_critic, obs_rms, args.env_name, (structure, connections), args.seed,
-                     args.num_processes, robot_dir_path, device)
+            determ_avg_reward = evaluate(
+                num_evals=Config.num_evals,
+                actor_critic=actor_critic,
+                obs_rms=obs_rms,
+                env_name=Config.env_name,
+                robot_structure=(body, connections),
+                seed=Config.seed,
+                num_processes=Config.num_processes,
+                eval_log_dir=log_dir,
+                device=device
+            )
 
-            if en_print:
-                print(f'Evaluated using {args.num_evals} episodes. Mean reward: {np.mean(determ_avg_reward)}\n')
+            if Config.print_en:
+                print(f'Evaluated using {Config.num_evals} episodes. Mean reward: {np.mean(determ_avg_reward)}\n')
 
             if determ_avg_reward > max_determ_avg_reward:
                 max_determ_avg_reward = determ_avg_reward              
                 
-                if en_print: print(f'Saving {parameter_f_path} with avg reward {max_determ_avg_reward}\n')
-                torch.save([
-                    actor_critic,
-                    getattr(utils.get_vec_normalize(envs), 'obs_rms', None)
-                ], parameter_f_path)
+                if Config.print_en: print(f'Saving {actor_critic_file} with avg reward {max_determ_avg_reward}\n')
+                torch.save(
+                    [
+                        actor_critic,
+                        getattr(utils.get_vec_normalize(envs), 'obs_rms', None)
+                    ],
+                    actor_critic_file
+                )
 
         # return upon reaching the termination condition
         if not termination_condition == None:
             if termination_condition(j):
-                if en_print: print(f'met termination condition ({j})...terminating...\n')
+                if Config.print_en: print(f'met termination condition ({j})...terminating...\n')
                 return max_determ_avg_reward
 
 #python ppo_main_test.py --env-name "roboticgamedesign-v0" --algo ppo --use-gae --lr 2.5e-4 --clip-param 0.1 --value-loss-coef 0.5 --num-processes 1 --num-steps 128 --num-mini-batch 4 --log-interval 1 --use-linear-lr-decay --entropy-coef 0.01
--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,56 @@
 import argparse
+import torch
 
 class Config:
 
     env_name = "Walker-v0"
+    shape = (5, 5)
+    max_evaluations = 250
+    population_size = 25
+    mutation_rate = 0.1
+    elite_rate_high = 0.6
+    elite_rate_low = 0.1
+    inherit_en = False
+    print_en = True
+    algo = 'ppo'
+    gail = False
+    gail_experts_dir = './gail_experts'
+    gail_batch_size = 128
+    gail_epoch = 5
+    lr = 2.5e-4
+    eps = 1e-5
+    alpha = 0.99
+    gamma = 0.99
+    use_gae = True
+    gae_lambda = 0.95
+    entropy_coef = 0.01
+    value_loss_coef = 0.5
+    max_grad_norm = 0.5
+    seed = 1
+    cuda_deterministic = False
+    max_iters = 1000
+    num_processes = 4
+    num_steps = 128
+    ppo_epoch = 4
+    num_mini_batch = 4
+    clip_param = 0.1
+    log_interval = 10
+    save_interval = 100
+    num_evals = 1
+    eval_interval = 50
+    num_env_steps = 10e6
+    log_dir = '/tmp/gym/'
+    save_dir = './trained_models/'
+    use_proper_time_limits = False
+    recurrent_policy = False
+    use_linear_lr_decay = True
+
+    cuda = torch.cuda.is_available()
+
+    assert algo in ['a2c', 'ppo', 'acktr']
+    # assert (args.from_middle == False or args.from_gen1 == False)
+    # if args.from_gen1:
+    #     assert args.gen0_experiment_dir is not None
+    if recurrent_policy:
+        assert algo in ['a2c', 'ppo'], \
+            'Recurrent policy is not implemented for ACKTR'
